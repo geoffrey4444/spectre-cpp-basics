0114dc5d1187f7585283edf6b5cc687e93152c55
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-06-01 12:56:40 -0700

    Fix headers

--- Examples/Day1/integrate/Left.hpp
+++ Examples/Day1/integrate/Left.hpp
@@ -2,5 +2,5 @@
 
 /// Integrate function `func` using the left point rule, using constant spacing
 /// `dx` and limits `a` and `b`
-double left(const std::function<double(const double)>& func, const double dx,
-            const double a, const double b) noexcept;
+double left(const std::function<double(double)>& func, double dx,
+            double a, double b) noexcept;
